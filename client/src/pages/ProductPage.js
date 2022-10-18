d4333976d8011582aeb334f7edb2fd009be9e338
andreearux99 <andreearux99@gmail.com>
2022-10-18 16:18:23 +0300

    added seller details to product page
    https://app.asana.com/0/1203056004334150/1203134034086017/f

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,22 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
-import magnetofon from '../data/magnetofon.jpg'
+import seller from '../data/magnetofon/seller.jpg';
+import m1 from '../data/magnetofon/m1.jpg';
+
+
 
 
 export default function ProductPage() {
       return (
         <>
           <div id="product-page">
-          <Form onSubmit="#" className="form-container">
-            <img src={magnetofon} alt="Magnetofon Akai GX-1730SS" />
 
-            <h1>Magnetofon Akai GX-1730SS</h1>
-            <h3>950 lei</h3>
-            <h5>Descriere</h5>
-            <p>Vand magnetofon Akai GX-1730SS,model foarte rar,in stare buna de functionare,estetic prezinta cateva zgarieturi,si pe capacul capetelor,am pus poze.</p>
+            <div id="product-description">
+              <img src={m1} alt="Magnetofon Akai GX-1730SS" />
+              <h1>Magnetofon Akai GX-1730SS</h1>
+              <h3>950 lei</h3>
+              <h5>Descriere</h5>
+              <p>Vand magnetofon Akai GX-1730SS,model foarte rar,in stare buna de functionare,estetic prezinta cateva zgarieturi,si pe capacul capetelor,am pus poze.</p>
+            </div>
 
-            </Form>
+            <div id="seller-details">
+              <div>
+                <img src={seller} alt="Negulescu Liviu" />
+                <p>
+                  <span>Negulescu Liviu</span><br />
+                  Pe Metaquantum din <b>iulie 2011</b><br/>
+                  Activ azi la 12:34
+                </p>
+              </div>
+
+              <div class="account-details">
+                <p>
+                  Intră în contul tău Metaquantum sau creează un cont nou pentru a contacta acest vânzător
+                  <br/><br/>
+                  <a href="#">Intra in cont</a><span> / </span><a href="#">Cont nou</a>
+                </p>
+              </div>
+
+              <div>
+                <button>Suna vanzatorul</button>
+                <button>Trimite mesaj</button>
+              </div>
+
+            </div>
+
           </div>
         </>
 )
